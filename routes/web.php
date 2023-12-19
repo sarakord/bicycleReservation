d3c3fb5895387af708e3ded47da32fb3dee851f6
sarakord <sarait405@gmail.com>
2023-12-19 12:10:01 +0000

    change readme

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    dd(\Illuminate\Support\Facades\Auth::user());
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
