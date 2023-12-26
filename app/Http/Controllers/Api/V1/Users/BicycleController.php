b02f52ca5938bb40c7ef3c580be02e1963d21b2c
s.kord <sarait405@gmail.com>
2023-12-26 12:57:32 +0330

    Added redis to project

--- app/Http/Controllers/Api/V1/Users/BicycleController.php
+++ app/Http/Controllers/Api/V1/Users/BicycleController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\V1\BicycleCollection;
 use App\Models\Bicycle;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class BicycleController extends Controller
 {
@@ -18,6 +19,15 @@ class BicycleController extends Controller
     {
         $bicycles = Bicycle::query()->latest()->active()
             ->paginate($request->query('limit') ?? 10);
+
+       /* $bicycles = Redis::set('bicycle_list', Bicycle::all());
+
+        dd(
+            json_decode(
+                \Illuminate\Support\Facades\Redis::get('bicycle_list'),
+                true
+            )
+        );*/
 
         return (new BicycleCollection($bicycles))
             ->response()
